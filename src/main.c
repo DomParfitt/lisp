afdb37871c4e7b4f8f1c8d9f71134878a62a5131
DomParfitt <dominicparfitt@economist.com>
2019-03-21 17:01:20 +0000

    Added main

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lexer/lexer.h"
+#include "lexer/token.h"
+#include "parser/parser.h"
+#include "parser/tree.h"
+#include "repl.h"
+
+int main() { return repl(); }
