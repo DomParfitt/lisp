83633beffda0c939a73793bcb7b91b5e2530b4d0
DomParfitt <dominicparfitt@economist.com>
2019-03-22 17:55:33 +0000

    Updated map implementation

--- include/interpreter/map.h
+++ include/interpreter/map.h
@@ -2,14 +2,22 @@
 #define MAP_H
 
 #include <stdlib.h>
+#include "type.h"
+
+typedef struct map_entry {
+  char* key;
+  type_struct value;
+} map_entry;
 
 typedef struct map {
   size_t size;
-  void** keys;
-  void* values[];
+  size_t count;
+  map_entry entries[];
 } map;
 
-void put(map map, void* key, void* value);
-void* get(map map, void* key);
+map* create_map(size_t size);
+void delete_map(map* map);
+void put(map map, char* key, type_struct value);
+type_struct* get(map map, char* key);
 
 #endif
--- src/interpreter/map.c
+++ src/interpreter/map.c
@@ -1,5 +1,37 @@
 #include "interpreter/map.h"
+#include <stdio.h>
+#include <string.h>
 
-void put(map map, void* key, void* value) {}
+map* create_map(size_t size) {
+  map* map = malloc(sizeof(map));
+  map->size = size;
+  map->count = 0;
+  map->entries[size];
+  return map;
+}
 
-void* get(map map, void* key);
+void delete_map(map* map) {
+  free(map->entries);
+  free(map);
+}
+
+void put(map map, char* key, type_struct value) {
+  for (size_t i = 0; i < map.count; i++) {
+    map_entry entry = map.entries[i];
+    if (strcmp(key, entry.key) == 0) {
+      entry.value = value;
+      return;
+    }
+  }
+  map.entries[map.count++] = (map_entry){key, value};
+}
+
+type_struct* get(map map, char* key) {
+  for (size_t i = 0; i < map.count; i++) {
+    map_entry entry = map.entries[i];
+    if (strcmp(key, entry.key) == 0) {
+      return &entry.value;
+    }
+  }
+  return NULL;
+}
