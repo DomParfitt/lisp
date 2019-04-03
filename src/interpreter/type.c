b5855ed96d4cd00c21b177013ae1ccd589c09f2d
DomParfitt <dominicparfitt@economist.com>
2019-04-03 14:39:29 +0100

    Returned pointer rather than struct

--- src/interpreter/type.c
+++ src/interpreter/type.c
@@ -1,7 +1,7 @@
 
 #include "interpreter/type.h"
 
-type_struct new_type_struct(type type, void* value) {
+type_struct* new_type_struct(type type, void* value) {
   type_val val;
   switch (type) {
     case BOOL:
@@ -17,5 +17,25 @@ type_struct new_type_struct(type type, void* value) {
       val.i = (int)value;
       break;
   }
-  return (type_struct){type, val};
+  type_struct* ts = malloc(sizeof(type_struct));
+  ts->type = type;
+  ts->value = val;
+  return ts;
+  // return (type_struct){type, val};
+}
+
+void print_type_struct(type_struct* t) {
+  switch (t->type) {
+    case BOOL:
+      printf("BOOL: %s\n", t->value.b == 0 ? "false" : "true");
+      break;
+    case INT:
+      printf("INT: %d\n", t->value.i);
+      break;
+    case STR:
+      printf("STR: %s\n", t->value.str);
+      break;
+    default:
+      printf("UNKNOWN: %d\n", t->value.i);
+  }
 }
