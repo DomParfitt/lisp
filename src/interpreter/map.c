c5a08a46069a81067cadc172b460d47a3b56f195
DomParfitt <dominicparfitt@economist.com>
2019-04-03 14:39:44 +0100

    Fixed put implementation

--- src/interpreter/map.c
+++ src/interpreter/map.c
@@ -25,13 +25,15 @@ void delete_map(map* map) {
 }
 
 void put(map* map, char* key, type_struct* value) {
-  map_entry* existing = get(map, key);
-  if (existing == NULL) {
-    map_entry* entry = create_map_entry(key, value);
-    map->entries[map->count++] = entry;
-  } else {
-    existing->value = value;
+  print_type_struct(&value);
+  for (size_t i = 0; i < map->count; i++) {
+    map_entry* entry = map->entries[i];
+    if (strcmp(key, entry->key) == 0) {
+      entry->value = value;
+      return;
+    }
   }
+  map->entries[map->count++] = create_map_entry(key, value);
 }
 
 type_struct* get(map* map, char* key) {
@@ -42,4 +44,17 @@ type_struct* get(map* map, char* key) {
     }
   }
   return NULL;
+}
+
+void print_map(map* map) {
+  printf("Map address %p\n", &map);
+  printf("Map has %d entries\n", map->count);
+  for (size_t i = 0; i < map->count; i++) {
+    map_entry* entry = map->entries[i];
+    printf("Entry #%lu\n", i);
+    printf("Key: %s\n", entry->key);
+    type_struct* t = entry->value;
+    print_type_struct(t);
+    printf("Type struct address: %p\n", &t);
+  }
 }
